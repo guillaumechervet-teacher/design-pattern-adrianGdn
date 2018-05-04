02a40e3f029781d73a42198463159fe2a7292df9
adrianGdn <adrian.gandon@epsi.fr>
2018-05-04 18:32:12 +0200

    End of day work - Git not commit error resolved

---
+++ Basket/ImperativeProgramming.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Basket
+{
+    public class ImperativeProgramming
+    {     
+        public static int CalculateBasketAmount(IList<BasketLineArticle> basketLineArticles)
+        {
+            var amountTotal = 0;
+            foreach (var basketLineArticle in basketLineArticles)
+            {
+                var id = basketLineArticle.Id;
+#if DEBUG
+                var article = GetArticleDatabaseMock(id);
+#else
+                var article = GetArticleFromDatabase(id);
+#endif
+                // Calculate amount
+                var amount = 0;
+                switch (article.Category)
+                {
+                    case "food":
+                        amount += article.Price * 100 + article.Price * 12;
+                        break;
+                    case "electronic": 
+                        amount += article.Price * 100 + article.Price * 20 + 4;
+                        break;
+                    case "desktop":
+                        amount += article.Price * 100 + article.Price * 20;
+                        break;
+                }
+
+                amountTotal += amount * basketLineArticle.Number;
+            }
+
+            return amountTotal;
+        }
+
+        private static ArticleDatabase GetArticleFromDatabase(string id)
+        {
+            // Retrive article from database
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var path = Uri.UnescapeDataString(uri.Path);
+            var assemblyDirectory = Path.GetDirectoryName(path);
+            var jsonPath = Path.Combine(assemblyDirectory, "article-database.json");
+            IList<ArticleDatabase> basketLineArticle = JsonConvert.DeserializeObject<List<ArticleDatabase>>(File.ReadAllText(jsonPath));
+            var article = basketLineArticle.First(articleDatabase => articleDatabase.Id == id);
+            return article;
+        }
+
+        public static ArticleDatabase GetArticleDatabaseMock(string id)
+        {
+            switch (id)
+            {
+                case "1":
+                    return new ArticleDatabase
+                    {
+                        Id = "1",
+                        Price = 1,
+                        Stock = 35,
+                        Label = "Banana",
+                        Category = "food"
+                    };
+                case "2":
+                    return new ArticleDatabase
+                    {
+                        Id = "2",
+                        Price = 500,
+                        Stock = 20,
+                        Label = "Fridge electrolux",
+                        Category = "electronic"
+                    };
+                case "3":
+                    return new ArticleDatabase
+                    {
+                        Id = "3",
+                        Price = 49,
+                        Stock = 68,
+                        Label = "Chair",
+                        Category = "desktop"
+                    };
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
