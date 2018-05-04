bf08bb5e027d85d6e9e60c3c6772cca6b80ed51d
adrianGdn <adrian.gandon@epsi.fr>
2018-05-04 15:31:03 +0200

    Test now work !

--- BasketTest/BasketOperation_CalculateBasketAmoutShould.cs
+++ BasketTest/BasketOperation_CalculateBasketAmoutShould.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Basket;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace BasketTest
 {
@@ -40,9 +45,35 @@ namespace BasketTest
             [TestMethod]
             [DynamicData("Baskets")]
             public void ReturnCorrectAmoutGivenBasket(BasketTest basketTest)
-        {
+            {
             var amountTotal = 0;
-            Assert.AreEqual(amountTotal, basketTest.ExpectedPrice);
-        }
+                foreach (var basketLineArticle in basketTest.BasketLineArticles)
+                {
+                    // Retrive article from database
+                    var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                    var uri = new UriBuilder(codeBase);
+                    var path = Uri.UnescapeDataString(uri.Path);
+                    var assemblyDirectory = Path.GetDirectoryName(path);
+                    var jsonPath = Path.Combine(assemblyDirectory, "article-database.json");
+                    IList < ArticleDatabase > articleDatabases = JsonConvert.DeserializeObject<List<ArticleDatabase>>(File.ReadAllText(jsonPath));
+                    var article = articleDatabases.First(articleDatabase =>articleDatabase.Id == basketLineArticle.Id);
+                    // Calculate amount
+                    var amount = 0;
+                    switch (article.Category)
+                    {
+                        case "food":
+                            amount += article.Price * 100 + article.Price * 12;
+                            break;
+                        case "electronic":
+                            amount += article.Price * 100 + article.Price * 20 + 4;
+                            break;
+                        case "desktop":
+                            amount += article.Price * 100 + article.Price * 20;
+                            break;
+                    }
+                    amountTotal += amount * basketLineArticle.Number;
+                }
+                Assert.AreEqual(amountTotal, basketTest.ExpectedPrice);
+            }
     }
 }
